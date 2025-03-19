239894c947b1a08697ce487f6e9728674e03ab97
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-03-19 16:40:26 -0400

    css

--- src/Sections/ContactSection.jsx
+++ src/Sections/ContactSection.jsx
@@ -1,14 +1,15 @@
 import { forwardRef } from "react";
+import { Text } from "../Components/Text";
 export const ContactSection = forwardRef((props, ref) => {
   return (
     <div className={"contact-section"} ref={ref}>
       <div className={"contact-column"}>
-        <div className={"contact-heading"}>Location</div>
-        <div className={"contact-description"}>Brooklyn, New York</div>
+        <div className={"contact-heading"}><Text text={"Location"} /></div>
+        <div className={"contact-description"}><Text text={"Brooklyn, New York"} /></div>
       </div>
       <div className="contact-column">
-        <div className={"contact-heading"}> Email</div>
-        <div className={"contact-description"}>nkossally@gmail.com</div>
+        <div className={"contact-heading"}> <Text text={"Email"} /></div>
+        <div className={"contact-description"}> <Text text={"nkossally@gmail.com"} /></div>
       </div>
     </div>
   );
